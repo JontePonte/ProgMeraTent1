6590f18bd4f859c8f7e07fece46cfd564bbe4bc8
JontePonte <john_nordstrand@hotmail.com>
2023-03-22 12:48:08 +0100

    Added test file and edited gitignore

---
+++ test.py
@@ -0,0 +1,11 @@
+""" Test file to make sure everything works before the exams starts """
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("Hello World!")
+
+print(pd.__version__)
+print(np.__version__)
+print(plt.__package__)
